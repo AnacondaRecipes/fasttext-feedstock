87148416becc0b289e951a3847265a0c1ee6e9e2
Tomasz Beneś <tbenes@anaconda.com>
2025-02-17 10:54:25 +0000

    0.9.3

--- recipe/build.sh
+++
@@ -1,6 +0,0 @@
-# We need to override the Makefile's hardcoded "c++".
-make -j"${CPU_COUNT}" CXX="${CXX}"
-mkdir -p "${PREFIX}/bin"
-mv fasttext "${PREFIX}/bin"
-
-"${PYTHON}" -m pip install --no-deps --ignore-installed -v .
